fc40a33734a56f680f55917f7931ea3793f46668
Med Amine Rhallab <70241115+marhallab@users.noreply.github.com>
2021-08-05 12:48:12 -0400

    Simple pokemon card created (#1)

--- pokemonchallengeapp/src/views/NavButton.vue
+++ pokemonchallengeapp/src/views/NavButton.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="navButton">
-    <b-button variant="success">Nav button</b-button>
+    <pokemonCard :pokeName="'Poke'" :pokeHeight="100" :pokeWeight="50" :imageUrl="'https://picsum.photos/600/300/?image=25'"/>
   </div>
 </template>
 
 <script>
+import  pokemonCard  from "../components/PokemonCard.vue";
 
 export default {
   name: 'navButton',
   components: {
+      pokemonCard
   }
 }
 </script>
