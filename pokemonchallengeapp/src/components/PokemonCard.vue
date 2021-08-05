5b60f854f1341e3006e720b31e83781f2d9b0f75
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-08-05 12:47:21 -0400

    Simple pokemon card created

---
+++ pokemonchallengeapp/src/components/PokemonCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pokemonCard">
+    <b-card
+      :title="pokeName"
+      :img-src="imageUrl"
+      img-alt="Image"
+      style="max-width: 20rem"
+    >
+      <b-card-text> <b>Weight:</b> {{ pokeWeight }} </b-card-text>
+
+      <b-card-text> <b>Height:</b> {{ pokeHeight }} </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pokemonCard",
+  props: {
+    pokeName: String,
+    pokeWeight: Number,
+    pokeHeight: Number,
+    imageUrl: String,
+  },
+  components: {},
+};
+</script>
