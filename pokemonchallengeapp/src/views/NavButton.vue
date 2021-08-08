f65a1891e894b2ee5fdb060ea3f8a91a293bba80
Med Amine Rhallab <70241115+marhallab@users.noreply.github.com>
2021-08-08 04:49:59 -0400

    Navigation with the button is finally done. (#4)
    * formating code
    
    * Adding template + method for button show more
    
    The methods to show the first load is not working
    
    * The method finally works
    
    We show 50 pokemons at the inital load and 15 every click
    
    * Add instructions for the end user

--- pokemonchallengeapp/src/views/NavButton.vue
+++ pokemonchallengeapp/src/views/NavButton.vue
@@ -1,16 +1,97 @@
 <template>
   <div class="navButton">
-    <pokemonCard :pokeName="'Poke'" :pokeHeight="100" :pokeWeight="50" :imageUrl="'https://picsum.photos/600/300/?image=25'"/>
+
+    <h1> Please click on the button "show more" at the BOTTOM of the page to add more pokemons.</h1>
+    <br>
+    <p><b>If the button doesn't show, that means that you have reach the limit of the pokemons available (150 pokemons max).</b></p>
+    <div class="pokemons-container">
+      <div
+        v-for="pok in allPokemons.slice(0, this.currentNumberDisplay)"
+        :key="pok.pokeName"
+      >
+        <pokemonCard
+          :pokeName="pok.pokeName"
+          :pokeHeight="pok.pokeHeight"
+          :pokeWeight="pok.pokeWeight"
+          :imageUrl="pok.imageUrl"
+        />
+      </div>
+    </div>
+    <h2> Current number of pokemons displayed: {{currentNumberDisplay}}</h2>
+    <br>
+    <b-button v-show="btnIsOn" v-on:click="updateListPokemons">Show More</b-button>
   </div>
 </template>
 
+<style scoped>
+
+.navButton {
+  margin-bottom: 10% ;
+}
+.pokemons-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+}
+</style>
+
 <script>
-import  pokemonCard  from "../components/PokemonCard.vue";
+import pokemonCard from "../components/PokemonCard.vue";
+import getRequestPokemons from "../components/GetRequestsPokemons";
 
 export default {
-  name: 'navButton',
+  name: "navButton",
+  data: function () {
+    return {
+      allPokemons: [],
+      allPokemonsDisplay: [],
+      btnIsOn: true,
+      currentNumberDisplay: 50,
+    };
+  },
   components: {
-      pokemonCard
-  }
-}
+    pokemonCard,
+  },
+
+  methods: {
+    getListPokemon() {
+      this.allPokemons = getRequestPokemons.getListPokemons();
+      //this.firstLoad();
+    },
+
+    firstLoad() {
+      /* for (var i = 0; i < 50; i++) {
+        console.log(this.allPokemons);
+        console.log(this.allPokemons.pop());
+        this.allPokemonsDisplay.push(this.allPokemons.pop());
+      }*/
+
+      this.allPokemonsDisplay = this.allPokemons.slice(
+        (0, this.currentNumberDisplay)
+      );
+    },
+
+    updateListPokemons() {
+      var lengthListDisplay = this.allPokemons.slice(
+        0,
+        this.currentNumberDisplay
+      ).length;
+      var lengthList = this.allPokemons.length;
+
+      if (lengthListDisplay != 150) {
+        if (lengthList - lengthListDisplay >= 15) {
+          this.currentNumberDisplay = this.currentNumberDisplay + 15;
+        } else if (lengthList - lengthListDisplay < 14) {
+          this.currentNumberDisplay =
+            this.currentNumberDisplay + (lengthList - lengthListDisplay);
+            this.btnIsOn = false;
+        }
+      } 
+    },
+  },
+
+  created: function () {
+    this.getListPokemon();
+  },
+};
 </script>
