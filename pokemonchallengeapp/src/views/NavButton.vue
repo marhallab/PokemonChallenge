e029850490be654396fd85cb7efda276dd395f3c
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-08-08 04:45:02 -0400

    The method finally works
    We show 50 pokemons at the inital load and 15 every click

--- pokemonchallengeapp/src/views/NavButton.vue
+++ pokemonchallengeapp/src/views/NavButton.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="navButton">
+
+    <h1> Please click on the button "show more" at the bottom of the page to add more pokemons</h1>
+    <br>
     <div class="pokemons-container">
-      <div v-for="pok of allPokemonsDisplay" :key="pok.pokeName">
+      <div
+        v-for="pok in allPokemons.slice(0, this.currentNumberDisplay)"
+        :key="pok.pokeName"
+      >
         <pokemonCard
           :pokeName="pok.pokeName"
           :pokeHeight="pok.pokeHeight"
@@ -10,13 +16,17 @@
         />
       </div>
     </div>
-    <b-button v-show="btnIsOn" v-on:click="updateListPokemons"
-      >Show More</b-button
-    >
+    <h2> Current number of pokemons displayed: {{currentNumberDisplay}}</h2>
+    <br>
+    <b-button v-show="btnIsOn" v-on:click="updateListPokemons">Show More</b-button>
   </div>
 </template>
 
 <style scoped>
+
+.navButton {
+  margin-bottom: 10% ;
+}
 .pokemons-container {
   display: flex;
   flex-flow: row wrap;
@@ -35,6 +45,7 @@ export default {
       allPokemons: [],
       allPokemonsDisplay: [],
       btnIsOn: true,
+      currentNumberDisplay: 50,
     };
   },
   components: {
@@ -44,37 +55,42 @@ export default {
   methods: {
     getListPokemon() {
       this.allPokemons = getRequestPokemons.getListPokemons();
-      this.firstLoad();
+      //this.firstLoad();
     },
 
     firstLoad() {
-      for (var i = 0; i < 50; i++) {
+      /* for (var i = 0; i < 50; i++) {
         console.log(this.allPokemons);
         console.log(this.allPokemons.pop());
         this.allPokemonsDisplay.push(this.allPokemons.pop());
-      }
+      }*/
+
+      this.allPokemonsDisplay = this.allPokemons.slice(
+        (0, this.currentNumberDisplay)
+      );
     },
 
     updateListPokemons() {
-      if (this.allPokemons.length != 0) {
-        if (this.allPokemons.length >= 15) {
-          for (var i = 0; i < 15; i++) {
-            this.allPokemonsDisplay.push(this.allPokemons.pop());
-          }
-        } else if (this.allPokemons.length < 14) {
-          for (var j = 0; j < 15; j++) {
-            this.allPokemonsDisplay.push(this.allPokemons.pop());
-          }
+      var lengthListDisplay = this.allPokemons.slice(
+        0,
+        this.currentNumberDisplay
+      ).length;
+      var lengthList = this.allPokemons.length;
+
+      if (lengthListDisplay != 150) {
+        if (lengthList - lengthListDisplay >= 15) {
+          this.currentNumberDisplay = this.currentNumberDisplay + 15;
+        } else if (lengthList - lengthListDisplay < 14) {
+          this.currentNumberDisplay =
+            this.currentNumberDisplay + (lengthList - lengthListDisplay);
+            this.btnIsOn = false;
         }
-      } else {
-        this.btnIsOn = false;
-      }
+      } 
     },
   },
 
   created: function () {
     this.getListPokemon();
-   
   },
 };
 </script>
